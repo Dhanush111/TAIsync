2ae385bfb7fe5cdf3bdf391ef3cce0d2b581e5e9
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-02 19:04:39 +0530

    Add or update Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java

--- Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
+++ Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
@@ -43,29 +43,3 @@ public class Main {
         System.out.print("No common odd elements found.");
        }
        else{
-        if(arr[i]%2!=0 && arr2[j]%2!=0){
-            if(arr[i]==arr2[j]){
-                al.add(arr[i]);
-                i++;
-                j++;
-            }
-            else if(arr[i]<arr2[j]){
-                i++;
-            }
-            else {
-                j++;
-            }
-        }
-        else{
-            if(arr[i]%2==0){
-                i++;
-            }
-            if(arr2[j]%2==0){
-                j++;
-            }
-        }
-       }
-       if(al.isEmpty()){
-        System.out.print("No common odd elements found.");
-       }
-       else{

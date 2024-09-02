66e0841c590a526b1f19cafca163015f617a30a8
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-03 01:22:37 +0530

    Add or update Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java

--- Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
+++ Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
@@ -18,11 +18,26 @@ public class Main {
        int i=0,j=0;
        while(i<n && j<m){
         if(arr[i]%2!=0 && arr2[j]%2!=0){
-       else{
-      for(int num:al){
-        System.out.print(num+" ");
-      }
-      }
-    }
-}
-//test
+        }
+        int m=scanner.nextInt();
+        int[] arr2=new int[m];
+        for(int j=0;j<m;j++){
+            arr2[j]=scanner.nextInt();
+        }
+       ArrayList<Integer> al=new ArrayList<>();
+       int i=0,j=0;
+       while(i<n && j<m){
+        if(arr[i]%2!=0 && arr2[j]%2!=0){
+            if(arr[i]==arr2[j]){
+                al.add(arr[i]);
+                i++;
+                j++;
+            }
+            else if(arr[i]<arr2[j]){
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        else{

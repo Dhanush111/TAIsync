8f9e06c2e29b82be7d758d3579b434550742c3f9
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-02 16:53:23 +0530

    Add Odd and Multiple of 7 Checker/code.java

---
+++ Odd and Multiple of 7 Checker/code.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.print((num%2!=0 && num%7==0)?"Yes":"No");
+    }
+}

0f869d89eada3bb4d0e11f05cc937aab35c6df15
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-03 00:22:55 +0530

    Add or update Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java

--- Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
+++ Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
@@ -1,1 +1,43 @@
-//test
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        int m=scanner.nextInt();
+        int[] arr2=new int[m];
+        for(int j=0;j<m;j++){
+            arr2[j]=scanner.nextInt();
+        }
+       ArrayList<Integer> al=new ArrayList<>();
+       int i=0,j=0;
+       while(i<n && j<m){
+        if(arr[i]%2!=0 && arr2[j]%2!=0){
+        }
+        int m=scanner.nextInt();
+        int[] arr2=new int[m];
+        for(int j=0;j<m;j++){
+            arr2[j]=scanner.nextInt();
+        }
+       ArrayList<Integer> al=new ArrayList<>();
+       int i=0,j=0;
+       while(i<n && j<m){
+        if(arr[i]%2!=0 && arr2[j]%2!=0){
+            if(arr[i]==arr2[j]){
+                al.add(arr[i]);
+                i++;
+                j++;
+            }
+            else if(arr[i]<arr2[j]){
+                i++;
+            }
+            else {
+                j++;
+            }
+        }
+        else{

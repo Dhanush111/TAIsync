94064cbe5a4920ffdecb125055adade0ff5e1ca4
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-02 12:21:36 +0530

    Add questions/First N Odds/code.java

---
+++ questions/First N Odds/code.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    for(int i=0;i<n;i++){
+      System.out.print(2*i+1 +" ");
+    }
+  }
+}

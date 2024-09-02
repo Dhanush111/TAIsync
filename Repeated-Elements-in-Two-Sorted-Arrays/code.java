a2452e8b88df2a85563f43f8a3407f595d8d6650
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-03 01:36:56 +0530

    Add or update Repeated-Elements-in-Two-Sorted-Arrays/code.java

--- Repeated-Elements-in-Two-Sorted-Arrays/code.java
+++ Repeated-Elements-in-Two-Sorted-Arrays/code.java
@@ -1,22 +1,23 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n=scanner.nextInt();
+while(i<n && j<m){
+            if(arr[i]==arr2[j]){
+        List<Integer> al=new ArrayList<>();
+        int i=0,j=0;
+        int arr2[]=new int[m];
+        for(int i=0;i<m;i++){
+            arr2[i]=scanner.nextInt();
+        }
         int[] arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
         }
         int m=scanner.nextInt();
-        int arr2[]=new int[m];
-        for(int i=0;i<m;i++){
-            arr2[i]=scanner.nextInt();
-        }
-        List<Integer> al=new ArrayList<>();
-        int i=0,j=0;
-        while(i<n && j<m){
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n=scanner.nextInt();
+import java.util.List;
+import java.util.Scanner;
+public class Main {
+import java.util.ArrayList;
             if(arr[i]==arr2[j]){
                 al.add(arr[i]);
                 i++;

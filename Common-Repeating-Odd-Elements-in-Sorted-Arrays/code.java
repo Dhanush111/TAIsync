a9cc5c0fa02614aa8c8a0753376616c6464fe10a
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-02 19:14:14 +0530

    Add or update Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java

--- Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
+++ Common-Repeating-Odd-Elements-in-Sorted-Arrays/code.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Collections;
+
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n=scanner.nextInt();
@@ -10,11 +12,137 @@ public class Main {
             arr[i]=scanner.nextInt();
         }
         int m=scanner.nextInt();
+
         int[] arr2=new int[m];
         for(int j=0;j<m;j++){
             arr2[j]=scanner.nextInt();
         }
+
        ArrayList<Integer> al=new ArrayList<>();
+
+       int i=0,j=0;
+       while(i<n && j<m){
+        if(arr[i]%2!=0 && arr2[j]%2!=0){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        int m=scanner.nextInt();
+
+        int[] arr2=new int[m];
+        for(int j=0;j<m;j++){
+            arr2[j]=scanner.nextInt();
+        }
+
+       ArrayList<Integer> al=new ArrayList<>();
+
        int i=0,j=0;
        while(i<n && j<m){
         if(arr[i]%2!=0 && arr2[j]%2!=0){
@@ -27,45 +155,3 @@ public class Main {
                 i++;
             }
             else {
-                j++;
-            }
-        }
-        else{
-            if(arr[i]%2==0){
-                i++;
-            }
-            if(arr2[j]%2==0){
-                j++;
-            }
-        }
-       }
-       if(al.isEmpty()){
-        System.out.print("No common odd elements found.");
-       }
-       else{
-        if(arr[i]%2!=0 && arr2[j]%2!=0){
-            if(arr[i]==arr2[j]){
-                al.add(arr[i]);
-                i++;
-                j++;
-            }
-            else if(arr[i]<arr2[j]){
-                i++;
-            }
-            else {
-                j++;
-            }
-        }
-        else{
-            if(arr[i]%2==0){
-                i++;
-            }
-            if(arr2[j]%2==0){
-                j++;
-            }
-        }
-       }
-       if(al.isEmpty()){
-        System.out.print("No common odd elements found.");
-       }
-       else{

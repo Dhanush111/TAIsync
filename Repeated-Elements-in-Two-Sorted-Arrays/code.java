60e913f8c9f4d4f39a38c2995441f8b02d177c1a
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-03 11:57:58 +0530

    Add or update Repeated-Elements-in-Two-Sorted-Arrays/code.java

--- Repeated-Elements-in-Two-Sorted-Arrays/code.java
+++ Repeated-Elements-in-Two-Sorted-Arrays/code.java
@@ -1,8 +1,10 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        int n=scanner.nextInt();
         int[] arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
@@ -12,24 +14,22 @@ public class Main {
         for(int i=0;i<m;i++){
             arr2[i]=scanner.nextInt();
         }
-        repEle(arr,arr2);
-    }
-    static void repEle(int[] arr, int[] arr2){
-        // List<Integer> al=new ArrayList<>();
-        int i=arr.length-1,j=0;
-        while(i>=0 && j<arr2.length){
+        ArrayList<Integer> al=new ArrayList<>();
+        int i=0,j=0;
+        while(i<arr.length &&j<arr2.length){
             if(arr[i]==arr2[j]){
-                System.out.print(arr2[j]+" ");
-                i--;
+                al.add(arr[i]);
+                i++;
                 j++;
             }else if(arr[i]<arr2[j]){
-                i--;
-            }else{
+                i++;
+            }
+            else{
                 j++;
             }
+        }
+       for(int a:al){
+        System.out.print(a+" ");
+       }
     }
-        // for(int a:al){
-        //     System.out.print(a+" ");
-        // }
-}
 }

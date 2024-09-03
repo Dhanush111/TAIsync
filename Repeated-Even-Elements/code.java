9655445a8af130d6dac1da8f0588415c0b397875
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-09-03 12:10:46 +0530

    Add or update Repeated-Even-Elements/code.java

---
+++ Repeated-Even-Elements/code.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        int m=scanner.nextInt();
+        int[] arr2=new int[m];
+        for(int i=0;i<m;i++){
+        arr2[i]=scanner.nextInt();
+        }
+        ArrayList<Integer> al=new ArrayList<>();
+        int i=0,j=0;
+        while(i<arr.length &&j<arr2.length){
+            if(arr[i]==arr2[j] && arr[i]%2==0){
+                al.add(arr[i]);
+                i++;
+                j++;
+            }
+            else if(arr[i]<arr2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+       for(int a:al){
+        System.out.print(a+" ");
+       }
+    }
+}

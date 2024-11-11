aed115570fcdfa4f45d3e6a05bb3cd47ca6677e9
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-11 14:33:06 +0530

    Add or update Add-2-Integers/code.java

---
+++ Add-2-Integers/code.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a=scanner.nextInt();
+    int b=scanner.nextInt();
+    System.out.print(a+b);
+    // dsfsdf
+    // sdf
+    // sdfsd
+    // f
+    // sdfg
+    // erg
+    // g
+    // hd
+    // hddf
+    // dsfsdfdf
+    // hdgd
+    // fg
+    // dfg
+    // d
+    // fgfd
+    // gdfg
+    // dfgg
+    // dfgg
+    // dgdf
+    // gdfgfg
+    // dfggdf
+    // gdfgfdger
+    // e
+    // gdfgfdgerg
+    // ergeg
+    // gdfgfdgerg
+    // gdfgfdgergd
+    // fgfdg
+    // dfggdfd
+    // gdfgfgfg
+    // dgdfdg
+    // dfggdfg
+    // dfg
+    // gdfgfdgerg
+  }
+}

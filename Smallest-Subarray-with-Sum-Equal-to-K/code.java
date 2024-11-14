5f9fd964c5721a57d94f4e3d3d6ab3eb0b086784
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-15 00:08:59 +0530

    Add or update Smallest-Subarray-with-Sum-Equal-to-K/code.java

---
+++ Smallest-Subarray-with-Sum-Equal-to-K/code.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int k=scanner.nextInt();
+        int n=scanner.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scanner.nextInt();
+        }
+        boolean found=false;
+        for(int size=1;size<=n;size++){
+            for(int i=0;i<=n-size;i++){
+                int sum=0;
+                for(int j=i;j<i+size;j++){
+                    sum+=arr[j];
+                }
+                if(sum==k){
+                    for(int j=i;j<i+size;j++){
+                        System.out.print(arr[j]+" ");
+                        found=true;
+                    }
+                    return;
+                }
+            }
+        }
+        if(!found){
+            System.out.print("No subarray found.");
+        }
+    }
+}

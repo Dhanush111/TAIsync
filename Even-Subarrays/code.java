42006a9ff5c0df5f01be41d40adc9d92c27a3289
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-14 23:42:55 +0530

    Add or update Even-Subarrays/code.java

--- Even-Subarrays/code.java
+++ Even-Subarrays/code.java
@@ -2,27 +2,22 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean found=false;
         int n=scanner.nextInt();
         int[] arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=scanner.nextInt();
         }
-        int[] presum=new int[arr.length+1];
-        for(int i=1;i<=arr.length;i++){
-            presum[i]=presum[i-1]+arr[i-1];
-        }
-        for(int size=1;size<=arr.length;size++){
-        for(int j=0;j<=arr.length-size;j++){
-            int sum=presum[j+size]-presum[j];
-            if(sum%2==0){
-                for(int index=j;index<j+size;index++){
-                    System.out.print(arr[index]+" ");
+        boolean found=false;
+        for(int size=1;size<=n;size++){
+            for(int i=0;i<=n-size;i++){
+                if(size%2==0){
+                for(int j=i;j<i+size;j++){
+                    System.out.print(arr[j]+" ");
                     found=true;
                 }
                 System.out.println();
+                }
             }
-        }
         }
         if(!found){
             System.out.print("None");

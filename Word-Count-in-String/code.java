1973687fa1ad514ebea318ca7afde22bdfed387c
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-15 13:33:59 +0530

    Add or update Word-Count-in-String/code.java

---
+++ Word-Count-in-String/code.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String [] arr=input.split(" +");
+        System.out.print(arr.length);
+    }
+}

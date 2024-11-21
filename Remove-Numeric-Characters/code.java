bc78485fb5094fc3dc108c581c7ef74c9cd798d5
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-21 23:45:55 +0530

    Add or update Remove-Numeric-Characters/code.java

---
+++ Remove-Numeric-Characters/code.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+       StringBuilder res=new StringBuilder();
+       for(int i=0;i<input.length();i++){
+        char c=input.charAt(i);
+        if(!(Character.isDigit(c))){
+            res.append(c);
+        }
+       }
+       System.out.print(res);
+    }
+}

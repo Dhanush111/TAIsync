c468124df7ae0f7d136c3cd7f7c5e4fef58fe02f
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-22 20:57:54 +0530

    Add or update Print-All-Substrings/code.java

--- Print-All-Substrings/code.java
+++ Print-All-Substrings/code.java
@@ -3,12 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();
-        for(int size =1;size<=str.length();size++){
+        for(int size=1;size<=str.length();size++){
             for(int i=0;i<=str.length()-size;i++){
-                for(int j=i;j<i+size;j++){
-                    System.out.print(str.charAt(j));
-                }
-                System.out.println();
+                System.out.println(str.substring(i,i+size));
             }
         }
     }

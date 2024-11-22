e503671cba8a3fb012e4c3ad3383e3b3c5686afa
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-22 21:29:43 +0530

    Add or update Largest-Word/code.java

---
+++ Largest-Word/code.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s=scanner.nextLine();
+        String[] arr=s.split(" ");
+        String ans=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(ans.length()<arr[i].length()){
+                ans=arr[i];
+            }
+        }
+        System.out.print(ans);
+    }
+}

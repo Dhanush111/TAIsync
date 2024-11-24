6db3da8fa070f7543e06a899a4053a9aef0e4590
dhanush <51503598+Dhanush111@users.noreply.github.com>
2024-11-24 23:31:00 +0530

    Add or update Duplicate-Elements-Finder/code.java

---
+++ Duplicate-Elements-Finder/code.java
@@ -0,0 +1,25 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.*;
+import java.util.Map.Entry;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        Map<Integer,Integer> map=new LinkedHashMap<>();
+        for(int a:arr){
+            map.put(a,(map.getOrDefault(a,0))+1);
+        }
+        Set<Entry<Integer,Integer>> s=map.entrySet();
+        for(Entry<Integer,Integer> e:s){
+            if(e.getValue()>1){
+                System.out.println(e.getKey());
+            }
+        }
+    }
+}
